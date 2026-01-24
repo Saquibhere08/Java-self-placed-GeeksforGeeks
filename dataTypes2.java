6dc6956d0ef4d34db84d0f6a6537a140f16403e0
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-01-24 16:46:07 +0530

    Add files via upload

---
+++ dataTypes2.java
@@ -0,0 +1,8 @@
+public class dataTypes2{
+    public static void main(String[] args) {
+        boolean isJavaFun=true;
+        boolean isFishTasty=false;
+        System.out.println("Is Java Fun? "+isJavaFun);
+        System.out.println("Is Fish tastu? "+isFishTasty);
+    }
+}
