d120853945b1599671ad792701ce13655e9b7e1b
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-02-02 11:57:50 +0530

    Add files via upload

---
+++ printStatements1.java
@@ -0,0 +1,7 @@
+public class printStatements1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java Programming.");
+        System.out.println("System.out.println prints text on a new line.");
+    }
+}
---
+++ printStatements2.java
@@ -0,0 +1,9 @@
+public class printStatements2 {
+    public static void main(String[] args) {
+        int num1=10,num2=20;
+
+        System.out.print("The addition of ");
+        System.out.print(num1 + " and "+num2+" is: ");
+        System.out.println(num1+num2);
+    }
+}
